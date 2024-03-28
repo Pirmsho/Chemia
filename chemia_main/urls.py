c260799dd3dffc871c7f3cd723729163bef1ddf7
Pirmsho <d.pirmisashvili@gmail.com>
2024-03-28 15:31:26 +0400

    added layout fragments

--- chemia_main/urls.py
+++ chemia_main/urls.py
@@ -6,7 +6,8 @@ app_name = 'chemia_main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
-    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('products/<int:category_id>/', views.products_by_category, name='products_by_category'),
+    # path('products/<int:product_id>/', views.product_detail, name='product_detail'),
     path('about/', views.about, name='about'),
     
 ]
--- chemia_main/views.py
+++ chemia_main/views.py
@@ -5,7 +5,13 @@ from .models import Product, Category
 
 def index(request):
     language = request.GET.get('lang', 'en')
-    return render(request, 'chemia_main/index.html', {'language': language})
+    belts_id = Category.objects.get(name='Leather Belt').id
+    bags_id = Category.objects.get(name='Leather Bag').id
+    wallet_id = Category.objects.get(name='Leather Wallet').id
+    return render(request, 'chemia_main/index.html', {'language': language,
+                                                      'belts': belts_id,
+                                                      'bags': bags_id,
+                                                      'wallets': wallet_id,})
 
 def products(request):
     language = request.GET.get('lang', 'en')
@@ -23,6 +29,16 @@ def product_detail(request, product_id):
         'product': product,
         'language': language,
         })
+
+def products_by_category(request, category_id):
+    language = request.GET.get('lang', 'en')
+    products = get_list_or_404(Product, category=category_id)
+    categories = get_list_or_404(Category)
+
+    return render(request, 'chemia_main/products.html', {'products': products,
+                                                         'language': language,
+                                                         'categories': categories,
+                                                         })
 
 def about(request):
     language = request.GET.get('lang', 'en')
